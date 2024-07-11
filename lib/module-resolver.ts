f726a5ae2baebc974721c7396207c2e7f7043223
amandesai01 <amandesai01@gmail.com>
2024-07-11 23:00:46 +0530

    feat: module resolver

---
+++ lib/module-resolver.ts
@@ -0,0 +1,107 @@
+import * as yml from 'js-yaml';
+
+import type { ModuleInfo } from './nuxt-modules/types';
+import defu from 'defu';
+import { fetchGithubPkg } from './nuxt-modules/utils';
+import { categories } from './nuxt-modules/categories';
+
+export async function generateModuleInfo(rawYmlContents: string, prNumber: number) {
+  let ymlContents: object;
+  try {
+    ymlContents = yml.load(rawYmlContents) as object;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Invalid YML Contents. YML Contents: ' + rawYmlContents);
+  }
+
+  let defuMod: ModuleInfo = {
+    name: '',
+    description: '',
+    repo: '', // nuxt/example
+    npm: '', // @nuxt/core
+    icon: '', // url or filename from /public/icons
+    github: '', // github link
+    website: '',
+    learn_more: '',
+    category: 'Devtools', // see modules/_categories.json
+    type: '3rd-party', // official, community, 3rd-party
+    maintainers: [],
+    compatibility: {
+      nuxt: '^2.0.0',
+      requires: {},
+    },
+  };
+
+  const mod = defu(ymlContents, defuMod);
+
+  if (!mod.repo) {
+    throw new Error(
+      'Module repo not found. YML Contents: ' +
+        rawYmlContents +
+        ' PR Number: ' +
+        prNumber,
+    );
+  }
+
+  const pkg = await fetchGithubPkg(mod.repo);
+
+  // Auto name
+  if (!mod.name) {
+    mod.name = (pkg.name.startsWith('@') ? pkg.name.split('/')[1] : pkg.name)
+      .replace('nuxt-', '')
+      .replace('-module', '');
+  }
+
+  if (!mod.github) {
+    mod.github = `https://github.com/${mod.repo.replace('#', '/tree/')}`;
+  }
+  if (!mod.website) {
+    mod.website = mod.github;
+  }
+
+  mod.npm = pkg.name || mod.npm;
+
+  // Type
+  if (
+    mod.repo.startsWith('nuxt-community/') ||
+    mod.repo.startsWith('nuxt-modules/')
+  ) {
+    mod.type = 'community';
+  } else if (mod.repo.startsWith('nuxt/')) {
+    mod.type = 'official';
+  } else {
+    mod.type = '3rd-party';
+  }
+
+  // Category
+  if (!mod.category) {
+    throw new Error(
+      'Module category not found. YML Contents: ' +
+        rawYmlContents +
+        ' PR Number: ' +
+        prNumber,
+    );
+  } else if (!categories.includes(mod.category)) {
+    let newCat = mod.category[0].toUpperCase() + mod.category.substr(1);
+    if (newCat.length <= 3) {
+      newCat = newCat.toUpperCase();
+    }
+    if (categories.includes(newCat)) {
+      mod.category = newCat;
+    } else {
+      throw new Error(
+        'Module category invalid. YML Contents: ' +
+          rawYmlContents +
+          ' PR Number: ' +
+          prNumber,
+      );
+    }
+  }
+
+  // Default description
+  if (!mod.description) {
+    mod.description = pkg.description
+  }
+
+  return mod;
+}
