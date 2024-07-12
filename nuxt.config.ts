3b045412a5596ebcc818db487c6fc27b5e692370
amandesai01 <amandesai01@gmail.com>
2024-07-13 03:11:32 +0530

    feat: nuxt app

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt Registry - Community',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      ],
+      htmlAttrs: {
+        'data-theme': 'forest',
+      },
+      bodyAttrs: {
+        class: 'font-secondary',
+      },
+    },
+  },
+  googleFonts: {
+    families: {
+      Raleway: [400, 500, 600, 700],
+      Mulish: [400, 500, 600, 700],
+    },
+    useStylesheet: true,
+  },
+  ssr: true,
+  srcDir: 'app',
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', 'nuxt-orama', 'nuxt-icon'],
+});
